a301b3dcf8ad7c38f93e65803728f3e88339c645
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-03-19 17:30:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use(express.json());
 
 app.use('/api/user', authRouter);
 app.use('/api/contacts', contactsRouter);
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
