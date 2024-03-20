81e8ba8d6971905921c29d26b3d54d504b50758d
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-03-20 12:43:20 +0100

    Update socketConfig.js

--- configs/socketConfig.js
+++ configs/socketConfig.js
@@ -1,8 +1,16 @@
 import { Server } from 'socket.io';
 import http from 'http';
+import cors from 'cors';
 
 const socketServer = http.createServer();
 const io = new Server(socketServer);
+
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST']
+};
+
+io.use(cors(corsOptions));
 
 io.on('connection', (socket) => {
   console.log('a User connected');
