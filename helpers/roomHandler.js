a490956c53bb55a8f4f7d19983457b604a416a1d
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-05-13 10:55:21 +0200

    added logic for searching and disconnect

--- helpers/roomHandler.js
+++ helpers/roomHandler.js
@@ -3,22 +3,22 @@ import { v4 as uuidV4 } from 'uuid';
 const rooms = {};
 
 export const roomHandler = (socket) => {
-    const createRoom = ({ peerId, selectedLanguage, selectedGender }) => {
+    const createRoom = ({ peerId, value, selectedLanguage, selectedGender, userName, userLanguage, userGender, userAge }) => {
 
         const existingRoom = Object.values(rooms).find(room => (
-            room.language === selectedLanguage && room.gender === selectedGender
+            room.language === userLanguage && room.gender === userGender &&  userAge >= value[0] &&userAge<=value[1]
         ));
 
         const availableRoom = Object.values(rooms).find(room => (
-            room.users.length < 2 
+            room.users.length < 2
         ));
 
         if (existingRoom) {
-            joinRoom({ roomId: existingRoom.roomId, peerId, selectedLanguage, selectedGender });
+            joinRoom({ roomId: existingRoom.roomId, peerId, selectedLanguage, selectedGender, userName,  userLanguage, userGender, userAge});
             return;
         }
         if (availableRoom) {
-            joinRoom({ roomId: availableRoom.roomId, peerId, selectedLanguage, selectedGender });
+            joinRoom({ roomId: availableRoom.roomId, peerId, selectedLanguage, selectedGender, userName });
             return
         }
 
@@ -26,35 +26,46 @@ export const roomHandler = (socket) => {
         rooms[roomId] = {
             roomId,
             users: [peerId],
+            names: [userName],
             language: selectedLanguage,
             gender: selectedGender
         };
         socket.emit("room-created", { roomId });
         console.log("User created the room", roomId);
-        joinRoom({ roomId, peerId, selectedLanguage, selectedGender });
+        joinRoom({ roomId, peerId, selectedLanguage, selectedGender, userName });
         console.log(rooms);
     };
 
-    const joinRoom = ({ roomId, peerId, selectedLanguage, selectedGender }) => {
+    const joinRoom = ({ roomId, peerId, selectedLanguage, selectedGender, userName,  userLanguage, userGender, userAge }) => {
         const room = rooms[roomId];
         if (room) {
-            console.log("User joined the room", roomId, peerId);
+            console.log(`${userName} joined the room`, roomId, peerId);
             if (room.users.includes(peerId)) {
                 console.log("User is already in the room", roomId, peerId);
                 return;
             }
+            if (room.users.length >= 2) {
+                console.log("Room is already full", roomId);
+                return;
+            }
             room.users.push(peerId);
+            room.names.push(userName);
             socket.join(roomId);
             socket.emit("user-joined", { roomId, peerId });
             socket.emit("get-user", {
                 roomId,
                 users: room.users,
+                names: room.names,
             });
         } else {
             createRoom({ peerId, selectedLanguage, selectedGender });
         }
+        socket.on("end-call", ( ) => {
+            console.log(`${userName} left the room`, roomId, peerId);
+            leaveRoom({ roomId, peerId });
+        });
         socket.on("disconnect", () => {
-            console.log("User left the room", roomId, peerId);
+            console.log(`${userName} left the room`, roomId, peerId);
             leaveRoom({ roomId, peerId });
         });
     };
