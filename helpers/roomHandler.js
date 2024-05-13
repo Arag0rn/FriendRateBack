4b77fef4e8a84c6729b1f17bfd468becedc19bf2
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2024-05-13 15:43:04 +0300

    add reset password

--- helpers/roomHandler.js
+++ helpers/roomHandler.js
@@ -5,27 +5,27 @@ const MAX_USERS_PER_ROOM = 2;
 
 export const roomHandler = (socket) => {
 
-    const createRoom = ({peerId}) => {
+    const createRoom = ({ peerId }) => {
         const roomId = uuidV4();
         rooms[roomId] = [];
-        socket.emit("room-created", {roomId});
+        socket.emit("room-created", { roomId });
         console.log("user create the room", roomId);
         joinRoom({ roomId, peerId });
         console.log(rooms);
     }
-    const joinRoom = ({roomId, peerId}) => {
-        if (rooms[roomId]){
+    const joinRoom = ({ roomId, peerId }) => {
+        if (rooms[roomId]) {
             console.log("user join the room", roomId, peerId);
             rooms[roomId].push(peerId);
             socket.join(roomId);
-            socket.to(roomId).emit("user-joined", {peerId});
+            socket.to(roomId).emit("user-joined", { peerId });
             socket.emit("get-user", {
                 roomId,
                 users: rooms[roomId],
             });
-    } else {
-        createRoom({ peerId });
-    }
+        } else {
+            createRoom({ peerId });
+        }
         socket.on("disconnect", () => {
             console.log("user left the room", roomId, peerId);
             leaveRoom({ roomId, peerId })
@@ -39,6 +39,6 @@ export const roomHandler = (socket) => {
     };
 
     socket.on('create-room', createRoom);
-    socket.on("join-room",  joinRoom);
+    socket.on("join-room", joinRoom);
 
 }
