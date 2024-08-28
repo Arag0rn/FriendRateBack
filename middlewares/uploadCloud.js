94f80289608d12527d321c3c2af56f48c18ba015
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-08-28 12:58:08 +0200

    Update uploadCloud.js

--- middlewares/uploadCloud.js
+++ middlewares/uploadCloud.js
@@ -1,6 +1,7 @@
 import multer from "multer";
 import { v2 as cloudinary } from 'cloudinary';
 import { ALLOWED_FORMATS } from "../utils/constant.js";
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -13,18 +14,25 @@ cloudinary.config({
     api_secret: CLOUDINARY_API_SECRET,
 });
 
-console.log(CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET);
-
-const upload = multer({
-    dest: "uploads/"
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'avatars',
+        allowed_formats: ALLOWED_FORMATS,
+        transformation: [
+            { width: 320, height: 320, crop: "fill", gravity: "face" },
+            { quality: "auto" }
+        ],
+    },
 });
+
+const upload = multer({ storage });
+
 export const handleUpload = (req, res, next) => {
     upload.single("avatarURL")(req, res, (err) => {
         if (err) {
-
             return res.status(400).json({ error: err.message });
         }
-
         next();
     });
 };
@@ -35,22 +43,9 @@ export const uploadToCloudinary = async (req, res, next) => {
             return res.status(400).json({ error: "File not provided" });
         }
 
-
-        const result = await cloudinary.uploader.upload(req.file.path, {
-            folder: "avatars",
-            allowed_formats: ALLOWED_FORMATS,
-            transformation: [
-                { width: 320, height: 320, crop: "fill", gravity: "face" },
-                { quality: "auto" }
-            ]
-        });
-
-        req.cloudinaryUrl = result.secure_url;
+        req.cloudinaryUrl = req.file.path;
         next();
     } catch (error) {
-        if (error.http_code === 400 && error.message.includes("file size")) {
-            return res.status(400).json({ error: "File size limit exceeded" });
-        }
         return res.status(400).json({ error: error.message });
     }
 };
